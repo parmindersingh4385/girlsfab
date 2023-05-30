d9490c97a7710123dd7d038c345e147f6dd0838d
Parminder singh <58941157+parmindersingh4385@users.noreply.github.com>
2023-05-30 18:32:37 +0530

    Update Home.js at 6:32 pm

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,9 +7,9 @@ function Home() {
                 <div class="special">
                     <div class="container landingBanner">
                         <div class="row">
-                            <div class="col-md-4"><a href="#"><img src="https://girlsfab.herokuapp.com/women_banner.jpg" class="img-fluid"/></a></div>	
-                            <div class="col-md-4"><a href="#"><img src="https://girlsfab.herokuapp.com/man_banner.jpg" class="img-fluid"/></a></div>	
-                            <div class="col-md-4"><a href="#"><img src="https://girlsfab.herokuapp.com/kids_banner.jpg" class="img-fluid"/></a></div>	
+                            <div class="col-md-4"><a href="#"><img src="https://girlsfab.onrender.com/women_banner.jpg" class="img-fluid"/></a></div>	
+                            <div class="col-md-4"><a href="#"><img src="https://girlsfab.onrender.com/man_banner.jpg" class="img-fluid"/></a></div>	
+                            <div class="col-md-4"><a href="#"><img src="https://girlsfab.onrender.com/kids_banner.jpg" class="img-fluid"/></a></div>	
                         </div>					
                     </div>
                 </div>
